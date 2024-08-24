7a62bf7cb5b5ae0f0b3b58e65c27019194f7bbf1
WildZoe <dr.aleksic.ivana@gmail.com>
2024-08-24 22:29:33 +0200

    Save High Scores in Local Storage

--- end.js
+++ end.js
@@ -3,6 +3,18 @@ const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+
+console.log(highScores);
+
+localStorage.setItem("highScores", JSON.stringify([]));
+
+// console.log(JSON.parse(localStorage.getItem("highScores")));
+
+
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', e => {
@@ -15,4 +27,21 @@ username.addEventListener('keyup', e => {
 saveHighScore = e => {
     e.preventDefault();
     console.log('clicked the Save button');
+
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        // score: mostRecentScore,
+        name: username.value
+    };
+
+    highScores.push(score);
+
+    highScores.sort( (a, b) => b.score - a.score );
+
+    highScores.splice(MAX_HIGH_SCORES);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+
+    window.location.assign("/");
+    // console.log(highScores);
 }
